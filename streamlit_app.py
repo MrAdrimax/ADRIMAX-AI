c00e1387800a3c5c9e5dcf9ed3b7bf2c44a7cc4b
MrAdrimax <adrimaxnd@gmail.com>
2025-04-21 23:26:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,19 @@
 import streamlit as st
+import google.generativeai as genai
+
+# Configura tu clave de API de Google
+genai.configure(api_key="AIzaSyDb82ILBtthgNiDkDGa2MiPsPCnDZ3wWeY")
+
+# Inicializa el modelo
+model = genai.GenerativeModel("gemini-pro")
 
 # Título de la aplicación
-st.title("ADRIMAX-AI")
+st.title("Adrimax AI")
 
-# Texto de bienvenida
-st.write("¡Bienvenid@s a Adrimax AI")
+# Entrada de texto del usuario
+prompt = st.text_input("Introduce tu pregunta:")
 
-# Entrada de texto
-nombre = st.text_input("Ingresa tu nombre:")
-
-# Botón para mostrar el nombre
-if st.button("Mostrar nombre"):
-    st.write(f"Hola, {nombre}!")
+# Procesa la entrada cuando se proporciona un prompt
+if prompt:
+    respuesta = model.generate_content(prompt)
+    st.write(respuesta.text)
