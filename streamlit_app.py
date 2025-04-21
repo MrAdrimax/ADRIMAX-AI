e508955e2a08e03baa09a6274f5b494a5c5bdc30
MrAdrimax <adrimaxnd@gmail.com>
2025-04-21 23:24:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
-streamlit
-google-generativeai
-Pillow
-requests
+import streamlit as st
+
+# Título de la aplicación
+st.title("ADRIMAX-AI")
+
+# Texto de bienvenida
+st.write("¡Bienvenid@s a Adrimax AI")
+
+# Entrada de texto
+nombre = st.text_input("Ingresa tu nombre:")
+
+# Botón para mostrar el nombre
+if st.button("Mostrar nombre"):
+    st.write(f"Hola, {nombre}!")
